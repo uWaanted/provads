fb7b77be6e467696fc6667e64743d21bc080ff70
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-25 01:11:08 +0000

    troca de caminho

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <h1 className="text-white">Teste em branco</h1>
       </header>
       <div>
-        <img src="/workspaces/provads/public/img/banner.svg" alt="Banner"/>
+        <img src="public/img/banner.svg" alt="Banner"/>
       </div></>
   );
 }
