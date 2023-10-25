41957def42510211daed0ac6af28231363064e81
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-24 22:57:10 -0300

    banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 
 export default function Home() {
   return (
-    <><header>
-        <h1 className="text-white">Teste em branco</h1>
-      </header>
       <div>
-        <img src="public/img/banner.svg" alt="Banner"/>
-      </div></>
+        <img src="/img/banner.svg" alt="Descrição da Imagem" />
+      </div>
   );
 }
