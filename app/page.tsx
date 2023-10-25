13bea57a1b5d9fd837895db00466dc49c1dbc1a5
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-25 00:37:38 +0000

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <>
@@ -7,11 +5,8 @@ export default function Home() {
         <h1 className="text-white">Teste em branco</h1>
       </header>
       <div>
-        <Image
-          src="/workspaces/provads/public/img/banner.svg"
+        <img src="/workspaces/provads/public/img/banner.svg"
           alt="Descrição da imagem"
-          width={300} // Defina a largura desejada
-          height={200} // Defina a altura desejada
         />
       </div>
     </>
