ee581bad0c38de52c1283dd8294378e63861f022
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-24 23:04:56 -0300

    componente banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Banner from "./components/Banner";
 
 export default function Home() {
   return (
+    <div>
       <div>
-        <img src="/img/banner.svg" alt="Descrição da Imagem" />
+        <img src="/img/banner.svg" alt="Descrição da Imagem"/>
+      </div>
+      <div>
+        <h1> TESTE</h1>
+      </div>
       </div>
   );
 }
