8b0965a53474f42960a3d41fcd02be81bd975ef6
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-25 00:17:18 +0000

    import Image

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <><header>
-      <h1 className="text-white">Teste em branco</h1>
-    </header>
+    <>
+      <header>
+        <h1 className="text-white">Teste em branco</h1>
+      </header>
       <div>
-
-        <img src="public/banner.svg" alt="Descrição da imagem"/>
-      </div></>
-  )
+        <Image
+          src="/workspaces/provads/public/banner.svg"
+          alt="Descrição da imagem"
+          width={300} // Defina a largura desejada
+          height={200} // Defina a altura desejada
+        />
+      </div>
+    </>
+  );
 }
