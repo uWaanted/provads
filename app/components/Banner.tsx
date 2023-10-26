ad68a442a3ae4aaba895b6779d777a3995361526
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 19:50:50 -0300

    fix: Banner

---
+++ app/components/Banner.tsx
@@ -0,0 +1,7 @@
+export default function Banner() {
+    return (
+        <div>
+          <img src="/img/banner.svg" alt="Descrição da Imagem"/>
+        </div>
+    );
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,8 @@ import Banner from "./components/Banner";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <img src="/img/banner.svg" alt="Descrição da Imagem"/>
-      </div>
-      <div>
-        <h1> TESTE</h1>
-      </div>
-      </div>
+<>
+<Banner />
+</>
   );
 }
