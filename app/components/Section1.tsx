f16e88088bca55f0647a5edb04642a4e3228664c
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 20:39:04 -0300

    button for a

--- app/components/Section1.tsx
+++ app/components/Section1.tsx
@@ -6,9 +6,12 @@ export default function Section1() {
         <h1 className="text-lg">
           A (modular, highly tweakable) responsive one-page template designed by UTFPR and released for free under the Creative Commons.
         </h1>
-        <button className="bg-orange-500 text-white px-4 py-2 rounded hover:bg-light-blue-500 hover:text-white mt-4">
+        <a
+          href="#"
+          className="bg-orange-500 text-white px-4 py-2 rounded hover:bg-light-blue-500 hover:text-white mt-4 inline-block"
+        >
           Get Started
-        </button>
+        </a>
       </div>
       <div className="flex-1 text-right">
         <img src="/img/banner.svg" alt="Descrição da Imagem" />
