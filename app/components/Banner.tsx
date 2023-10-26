66925d76ed35d1c2c3efd2fb966d5b146b8fd872
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 20:22:59 -0300

    Section 1 Button

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,12 +1,15 @@
 export default function Banner() {
   return (
-    <div className="flex">
+    <div>
       <div className="flex-1 flex items-center">
-        <h1>TESTE</h1>
+        <h2>UTFPR</h2>
+        <h1> A (modular, highly tweakable) responsive one-page template designed by UTFPR  and released for free under the Creative Commons.</h1>
+        <Button> Get Started</Button> 
       </div>
       <div className="flex-1 text-right">
         <img src="/img/banner.svg" alt="Descrição da Imagem" />
       </div>
     </div>
+  
   );
 }
