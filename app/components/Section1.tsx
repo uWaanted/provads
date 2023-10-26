f2c4637f43e93bc3d7b31fe8ce82b3677c146bb4
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 20:50:05 -0300

    Section1 justify

--- app/components/Section1.tsx
+++ app/components/Section1.tsx
@@ -1,13 +1,13 @@
 export default function Section1() {
   return (
     <div className="flex">
-      <div className="flex-1 flex flex-col justify-center">
-        <h3 className="text-3xl font-bold">UTFPR</h3>
+      <div className="flex-1 flex flex-col ">
+        <h3 className="text-3xl font-bold justify-center">UTFPR</h3>
         <h1 className="text-lg">
           A (modular, highly tweakable) responsive one-page template designed by UTFPR and released for free under the Creative Commons.
         </h1>
         <a
-                        className="text-[#3d3d3d] bg-white px-2 py-[4px] rounded hover:bg-[#00e77f] hover:text-white"
+                        className=" bg-white b-2 rounded hover:bg-cyan hover:text-white justify-center"
                         href="#"
                     >
                         Get Started
