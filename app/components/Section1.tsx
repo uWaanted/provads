804102219b11be316d90c35366466e63e36f4fe5
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 20:43:10 -0300

    teste text-center a

--- app/components/Section1.tsx
+++ app/components/Section1.tsx
@@ -2,13 +2,13 @@ export default function Section1() {
   return (
     <div className="flex">
       <div className="flex-1 flex flex-col justify-center">
-        <h2 className="text-3xl font-bold">UTFPR</h2>
+        <h3 className="text-3xl font-bold">UTFPR</h2>
         <h1 className="text-lg">
           A (modular, highly tweakable) responsive one-page template designed by UTFPR and released for free under the Creative Commons.
         </h1>
         <a
           href="#"
-          className="bg-orange-500 text-white px-4 py-2 rounded hover:bg-light-blue-500 hover:text-white mt-4 inline-block"
+          className="bg-orange-500 text-white px-2 py-1 rounded hover:bg-light-blue-500 hover:text-white inline-block text-center"
         >
           Get Started
         </a>
