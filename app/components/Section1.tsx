fcc527199864a1533a04425766c195a47cee337b
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 20:27:35 -0300

    Button A

--- app/components/Section1.tsx
+++ app/components/Section1.tsx
@@ -4,7 +4,7 @@ export default function Section1() {
       <div className="flex-1 flex items-center">
         <h2>UTFPR</h2>
         <h1> A (modular, highly tweakable) responsive one-page template designed by UTFPR  and released for free under the Creative Commons.</h1>
-        <Button> Get Started</Button> 
+        <A class=" absolute top-0 right-4 mt-4 ml-4 text-[white] bg-orange-500 px-8 py-[8px] rounded hover:bg-[lightblue] hover:text-white"> Get Started</A> 
       </div>
       <div className="flex-1 text-right">
         <img src="/img/banner.svg" alt="Descrição da Imagem" />
