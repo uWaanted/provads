f9c7f89a9b0eae519480d304b8dbba793b133930
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 20:47:55 -0300

    novo botão a

--- app/components/Section1.tsx
+++ app/components/Section1.tsx
@@ -2,16 +2,16 @@ export default function Section1() {
   return (
     <div className="flex">
       <div className="flex-1 flex flex-col justify-center">
-        <h3 className="text-3xl font-bold">UTFPR</h2>
+        <h3 className="text-3xl font-bold">UTFPR</h3>
         <h1 className="text-lg">
           A (modular, highly tweakable) responsive one-page template designed by UTFPR and released for free under the Creative Commons.
         </h1>
         <a
-          href="#"
-          className="bg-orange-500 text-white px-2 py-1 rounded hover:bg-light-blue-500 hover:text-white inline-block text-center"
-        >
-          Get Started
-        </a>
+                        className="text-[#3d3d3d] bg-white px-2 py-[4px] rounded hover:bg-[#00e77f] hover:text-white"
+                        href="#"
+                    >
+                        Get Started
+                    </a>
       </div>
       <div className="flex-1 text-right">
         <img src="/img/banner.svg" alt="Descrição da Imagem" />
