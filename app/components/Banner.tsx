9c5a1ad7ec3c8871757740c62600d13567af2dae
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 20:09:20 -0300

    flex banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,12 +1,12 @@
 export default function Banner() {
   return (
-    <div>
-      <div className="text-left">
-        <h1> TESTE</h1>
+    <div className="flex">
+      <div className="flex-1 text-left">
+        <h1>TESTE</h1>
       </div>
-    <div className="text-right">
-      <img src="/img/banner.svg" alt="Descrição da Imagem" />
-    </div>
+      <div className="flex-1 text-right">
+        <img src="/img/banner.svg" alt="Descrição da Imagem" />
+      </div>
     </div>
   );
 }
