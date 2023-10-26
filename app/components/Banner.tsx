57a55128de68c75c8c2b6dddce1576c980dadfcf
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 20:07:13 -0300

    commit teste banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,7 +1,12 @@
 export default function Banner() {
   return (
+    <div>
+      <div className="text-left">
+        <h1> TESTE</h1>
+      </div>
     <div className="text-right">
       <img src="/img/banner.svg" alt="Descrição da Imagem" />
+    </div>
     </div>
   );
 }
