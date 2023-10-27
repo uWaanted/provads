1fe4bdd8b05a133abf5e961387512583dd909cb0
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 22:27:04 -0300

    Create: Icones

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 import Section4 from "./components/Section4";
+import Icones from "./components/Icones";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
 <Section2 />
 <Section3 />
 <Section4 />
+<Icones />
 </>
   );
 }
