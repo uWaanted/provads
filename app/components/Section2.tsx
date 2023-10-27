3dd10f8c7a9681e925046198a4c7549951083078
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 21:57:46 -0300

    Section2 left image

--- app/components/Section2.tsx
+++ app/components/Section2.tsx
@@ -1,14 +1,14 @@
 export default function Section2() {
     return (
-        <div className="wrapper md:flex items-center flex-row-reverse">
+      <div className="wrapper md:flex items-center flex-row-reverse">
         <div className="md:flex-1 text-left">
           <img src="/img/spotlight01.svg" alt="Spotlight" className="w-full" />
         </div>
         <div className="md:flex-1 flex flex-col justify-center">
           <h2 className="text-3xl text-center">Spotlight</h2>
           <p className="text-lg text-center">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. 
-          Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. 
+            Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.
           </p>
           <div className="text-center mt-4">
             <a
