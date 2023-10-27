c4e77fcf6c8823f879fdd047f884098af3156f55
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 21:15:24 -0300

    teste w-full

--- app/components/Section1.tsx
+++ app/components/Section1.tsx
@@ -1,18 +1,18 @@
 export default function Section1() {
   return (
     <div className="wrapper md:flex items-center flex-row-reverse">
-      <div className="flex-1 text-right">
-        <img src="/img/banner.svg" alt="Descrição da Imagem" />
+      <div className="md:flex-1 text-right">
+        <img src="/img/banner.svg" alt="Descrição da Imagem" className="w-full" />
       </div>
-      <div className="flex-1 flex flex-col justify-center">
+      <div className="md:flex-1 flex flex-col justify-center">
         <h2 className="text-3xl text-center">UTFPR</h2>
         <p className="text-lg text-center">
           A (modular, highly tweakable) responsive one-page template designed by UTFPR and released for free under the Creative Commons.
         </p>
-        <div className="text-center">
+        <div className="text-center mt-4">
           <a
             href="#"
-            className="border-2 border-black text-black px-4 py-2 rounded hover:border-cyan-500 hover:text-cyan-500 hover:bg-transparent mt-4 inline-block"
+            className="border-2 border-black text-black px-4 py-2 rounded hover:border-cyan-500 hover:text-cyan-500 hover:bg-transparent inline-block"
           >
             Get Started
           </a>
