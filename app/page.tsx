30200779b56adef466eafda79e7ad0ae58be2ef3
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-26 22:04:31 -0300

    create: Section3

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
+import Section3 from "./components/Section3";
 
 export default function Home() {
   return (
 <>
 <Section1 />
 <Section2 />
+<Section3 />
 </>
   );
 }
