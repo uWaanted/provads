ad4e85b9ae06b3fc7270ca813c16f6d74d5426c2
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-27 22:36:07 -0300

    botaozada

--- app/components/Section1.tsx
+++ app/components/Section1.tsx
@@ -10,12 +10,13 @@ export default function Section1() {
           A (modular, highly tweakable) responsive one-page template designed by UTFPR and released for free under the Creative Commons.
         </p>
         <div className="text-center mt-4">
-          <a
-            href="#"
-            className="border-2 border-black text-black px-4 py-2 rounded hover:border-cyan-500 hover:text-cyan-500 hover:bg-transparent inline-block"
-          >
-            Get Started
-          </a>
+        <a
+  href="#"
+  className="border-2 border-black text-black px-8 py-4 rounded-full hover:border-cyan-500 hover:text-cyan-500 hover:bg-transparent inline-block"
+>
+  Learn More
+</a>
+
         </div>
       </div>
     </div>
--- app/components/Section4.tsx
+++ app/components/Section4.tsx
@@ -11,12 +11,13 @@ export default function Section4() {
             Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.
           </p>
           <div className="text-center mt-4 ">
-            <a
-              href="#"
-              className="border-2 border-black text-black px-4 py-2 rounded hover:border-cyan-500 hover:text-cyan-500 hover:bg-transparent inline-block"
-            >
-              Learn More
-            </a>
+          <a
+  href="#"
+  className="border-2 border-black text-black px-8 py-4 rounded-full hover:border-cyan-500 hover:text-cyan-500 hover:bg-transparent inline-block"
+>
+  Learn More
+</a>
+
           </div>
         </div>
       </div>
